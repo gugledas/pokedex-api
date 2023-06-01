112b3f93950da7867df6a32ca90e486cb4f2c879
gugledas <azabzistany@gmail.com>
2023-06-01 18:47:08 +0100

    maj

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -5,66 +5,66 @@ const userModel = require("../models/user");
 let pokemons = require("./mock-pokemon");
 const bcrypt = require("bcrypt");
 
-//let sequelize;
+let sequelize;
 
-//if (process.env.NODE_ENV === "production") {
-console.log("node env", process.env.NODE_ENV);
-let sequelize = new Sequelize(
-  "tcqvgu87br3ns7aj",
-  "apxf9axcndlaf7n6",
-  "wf1yvhdl326mnrga",
-  {
-    host: "q0h7yf5pynynaq54.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize(
+    "tcqvgu87br3ns7aj",
+    "apxf9axcndlaf7n6",
+    "wf1yvhdl326mnrga",
+    {
+      host: "q0h7yf5pynynaq54.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      dialect: "mariadb",
+      dialectOptions: {
+        timezone: "GMT+1"
+      },
+      logging: false
+    }
+  );
+} else {
+  sequelize = new Sequelize("pokedex", "root", "stan", {
+    host: "127.0.0.1",
     dialect: "mariadb",
     dialectOptions: {
       timezone: "GMT+1"
     },
     logging: false
-  }
-);
-// } else {
-//   console.log("node local", process.env.NODE_ENV);
-//   sequelize = new Sequelize("pokedex", "root", "stan", {
-//     host: "127.0.0.1",
-//     dialect: "mariadb",
-//     dialectOptions: {
-//       timezone: "GMT+1"
-//     },
-//     logging: false
-//   });
-// }
+  });
+}
+
+sequelize
+  .authenticate()
+  .then((_) => console.log("Connexion réussi"))
+  .catch((error) =>
+    console.log(`Erreur lors de la connection à la Bd ${error}`)
+  );
 
 let Pokemon = pokemonModel(sequelize, DataTypes);
 let User = userModel(sequelize, DataTypes);
 
 const initDb = function () {
-  return sequelize
-    .sync({ force: true })
-    .then((_) => {
-      console.log("BD synchronizer");
-      pokemons.map((pokem, i) => {
-        Pokemon.create({
-          name: pokem.name,
-          hp: pokem.hp,
-          cp: pokem.cp,
-          picture: pokem.picture,
-          types: pokem.types
-        }).then((p) => console.log(i + 1 + " pokemon crée"));
-      });
-      /* Init one User */
-      bcrypt
-        .hash("2023", 10)
-        .then((hash) =>
-          User.create({
-            username: "gugledas",
-            password: hash
-          })
-        )
-        .then((user) => console.log(`User ${user.username} a été créer`));
-    })
-    .catch((error) =>
-      console.log(`Erreur lors de la connection à la Bd ${error}`)
-    );
+  return sequelize.sync({ force: true }).then((_) => {
+    console.log("BD synchronizer");
+    pokemons.map((pokem, i) => {
+      Pokemon.create({
+        name: pokem.name,
+        hp: pokem.hp,
+        cp: pokem.cp,
+        picture: pokem.picture,
+        types: pokem.types
+      }).then((p) => console.log(i + 1 + " pokemon crée"));
+    });
+    /* Init one User */
+    bcrypt
+      .hash("2023", 10)
+      .then((hash) =>
+        User.create({
+          username: "gugledas",
+          password: hash
+        })
+      )
+      .then((user) => console.log(`User ${user.username} a été créer`));
+  });
 };
 
 module.exports = {
